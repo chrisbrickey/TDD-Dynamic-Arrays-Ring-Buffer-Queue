1d622872ef52ed84444f66222cdb670a89d2c350
chrisbrickey <cbrickey1@gmail.com>
2017-10-22 16:18:19 -0700

    remove method names because not generated via TDD

--- lib/queue_with_max.rb
+++ lib/queue_with_max.rb
@@ -1,20 +1,4 @@
 require_relative 'ring_buffer'
 
 class QueueWithMax
-
-  def initialize
-  end
-
-  def enqueue(val)
-  end
-
-  def dequeue
-  end
-
-  def max
-  end
-
-  def length
-  end
-
 end
