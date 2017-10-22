c29aea2fb2dcf2c15c7e380877d5ae9cf3f36bdd
chrisbrickey <cbrickey1@gmail.com>
2017-10-22 16:16:43 -0700

    restructure tests under headings for public methods of the API

--- spec/dynamic_array_spec.rb
+++ spec/dynamic_array_spec.rb
@@ -1,11 +1,67 @@
 require "dynamic_array"
 
-describe DynamicArray do
+# I've listed the public methods required for my API and am writing tests for each of those methods prior to implementing them
 
+describe "DynamicArray#initialize" do
   it "starts out empty" do
     subject = DynamicArray.new
     expect(subject.length).to eq(0)
   end
+end
+
+describe "DynamicArray#[]" do
+
+  it "returns an item when given a negative index in bounds" do
+    subject = DynamicArray.new
+    subject.push("apple")
+    expect(subject[-1]).to eq("apple")
+  end
+
+  it "raises error when given a negative index out of bounds" do
+    subject = DynamicArray.new
+    subject.push("apple")
+    expect do
+      subject[-2]
+    end.to raise_error("index out of bounds")
+  end
+
+end
+
+describe "DynamicArray#[]=" do
+
+  it "changes an item at a given index" do
+    subject = DynamicArray.new
+    subject.push("apple")
+    subject[0] = "orange"
+    expect(subject[0]).to eq("orange")
+  end
+
+  it "raises error when asked to change an item at a given positive index out of bounds" do
+    subject = DynamicArray.new
+    expect do
+      subject[0] = "apple"
+    end.to raise_error("index out of bounds")
+  end
+
+  it "raises error when asked to change an item at a given negative index out of bounds" do
+    subject = DynamicArray.new
+    expect do
+      subject[-1] = "apple"
+    end.to raise_error("index out of bounds")
+  end
+
+end
+
+
+# Ultimately, I'd like to design the DynamicArray API
+# ...such that all methods run at constant time complexity (on average).
+# To test time complexity, I should be testing the actual time complexities experienced in this environment
+# ...by comparing my API method run times to methods with linear time complexity
+# If, instead, I tested whether or not a 'resize' helper method is called (doubles size of underlying array to acheive constant time on avg),
+# ...then I'm testing the implementation not the behavior. That misses the point of TDD.
+
+
+describe "DynamicArray#push" do
 
   it "accepts a new item" do
     subject = DynamicArray.new
@@ -18,6 +74,10 @@ describe DynamicArray do
     subject.push("apple")
     expect(subject[0]).to eq("apple")
   end
+
+end
+
+describe "DynamicArray#pop" do
 
   it "can remove an item and have correct length" do
     subject = DynamicArray.new
@@ -50,46 +110,10 @@ describe DynamicArray do
     expect(result).to eq("apple")
   end
 
-  it "returns an item when given a negative index in bounds" do
-    subject = DynamicArray.new
-    subject.push("apple")
-    expect(subject[-1]).to eq("apple")
-  end
+end
 
-  it "raises error when given a negative index out of bounds" do
-    subject = DynamicArray.new
-    subject.push("apple")
-    expect do
-      subject[-2]
-    end.to raise_error("index out of bounds")
-  end
+describe "DynamicArray#shift" do
+end
 
-  it "changes an item at a given index" do
-    subject = DynamicArray.new
-    subject.push("apple")
-    subject[0] = "orange"
-    expect(subject[0]).to eq("orange")
-  end
-
-  it "raises error when asked to change an item at a given positive index out of bounds" do
-    subject = DynamicArray.new
-    expect do
-      subject[0] = "apple"
-    end.to raise_error("index out of bounds")
-  end
-
-  it "raises error when asked to change an item at a given negative index out of bounds" do
-    subject = DynamicArray.new
-    expect do
-      subject[-1] = "apple"
-    end.to raise_error("index out of bounds")
-  end
-
-#TDD: If we want to test the time complexity of push/pop/shift/unshift,
-# ...we should be testing the actual time complexity (compare to linear time)
-# ...If I test whether or not a 'resize' helper method is called (doubles size of underlying array),
-# ...then I'm testing the implementation not the behavior - misses the point of testing
-
-
-
+describe "DynamicArray#unshift" do
 end
