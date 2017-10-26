a8b4cd166b7c08624772c95c527d461012780cb0
chrisbrickey <cbrickey1@gmail.com>
2017-10-26 09:15:07 -0700

    add testing structure for all Classes that require overwrite of hashing function

--- spec_hash/hashing_spec.rb
+++ spec_hash/hashing_spec.rb
@@ -3,6 +3,28 @@ require "rspec/expectations"
 
 describe "Hashing Functions" do
 
+  describe "Fixnum#hash" do
+
+    before(:all) do
+      subject = 5
+    end
+
+    it "should return an integer" do
+      expect(subject.hash).to be_a(Integer)
+    end
+  end # of Fixnum
+
+  describe "String#hash" do
+
+    before(:all) do
+      subject = "hello"
+    end
+
+    it "should return an integer" do
+      expect(subject.hash).to be_a(Integer)
+    end
+  end # of String
+
   describe "Array#hash" do
 
     before(:all) do
@@ -12,6 +34,17 @@ describe "Hashing Functions" do
     it "should return an integer" do
       expect(subject.hash).to be_a(Integer)
     end
-  end
+  end # of Array
+
+  describe "Hash#hash" do
+
+    before(:all) do
+      subject = { "a" => 5, "b" => [5, 6, 9] }
+    end
+
+    it "should return an integer" do
+      expect(subject.hash).to be_a(Integer)
+    end
+  end # of Hash
 
 end # of hashing
