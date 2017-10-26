9533f0c45521090d3da0cff4c6512e54fcaca5e4
chrisbrickey <cbrickey1@gmail.com>
2017-10-26 16:46:05 -0700

    correct typos in deterministic test for integer

--- spec_hash/hashing_spec.rb
+++ spec_hash/hashing_spec.rb
@@ -8,6 +8,18 @@ describe "Hashing Functions" do
     it "should return an integer" do
       expect(5.my_hash).to be_a(Integer)
     end
+
+    it "should hash in a deterministic way" do
+      expect(59.my_hash).not_to eq(95)
+      expect(95.my_hash).to eq(95.my_hash)
+    end
+
+    it "should handle zero" do
+      expect(0.my_hash).no to_eq(0)
+    end
+
+    it "should not return similar results for "
+
   end # of Fixnum
 
   describe "String#my_hash" do
