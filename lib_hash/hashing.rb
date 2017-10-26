bc572c5857b20df4cf78b666e41bd7ad37b748df
chrisbrickey <cbrickey1@gmail.com>
2017-10-26 09:35:08 -0700

    establish structure for hashing functions

---
+++ lib_hash/hashing.rb
@@ -0,0 +1,31 @@
+class Fixnum
+
+  def my_hash
+    "a"
+  end
+
+end
+#
+class Array
+
+  def my_hash
+    "a"
+  end
+
+end
+
+class Hash
+
+  def my_hash
+    "a"
+  end
+
+end
+
+class String
+
+  def my_hash
+    "a"
+  end
+
+end
