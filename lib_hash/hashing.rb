72d0ddf3fd3d545552076554ad1394af22671117
chrisbrickey <cbrickey1@gmail.com>
2017-10-26 09:52:40 -0700

    implement hash function for Integers

--- lib_hash/hashing.rb
+++ lib_hash/hashing.rb
@@ -1,11 +1,20 @@
-class Fixnum
+class Integer
 
   def my_hash
-    "a"
+    salt = 937
+    
+    result = 1
+    str = self.to_s
+    str.each_char.with_index do |char, idx|
+      result *= (salt * (char.ord + idx))
+    end
+
+    result
   end
 
 end
-#
+
+
 class Array
 
   def my_hash
