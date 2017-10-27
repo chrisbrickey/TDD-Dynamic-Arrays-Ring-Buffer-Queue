41e7d7f24d26db77436b9169ba81786d07e6a2be
chrisbrickey <cbrickey1@gmail.com>
2017-10-26 18:12:35 -0700

    write tests for strings

--- spec_hash/hashing_spec.rb
+++ spec_hash/hashing_spec.rb
@@ -18,8 +18,14 @@ describe "Hashing Functions" do
     end
 
     it "should handle negative integers" do
-      # p -34.hash
       expect(-34.my_hash).to be_a(Integer)
+    end
+
+    it "should not return a negative integer" do
+      expect(-34.my_hash).to be > 0
+      expect(104.my_hash).to be > 0
+      expect(-13.my_hash).to be > 0
+      expect(-1.my_hash).to be > 0
     end
 
     it "should not return similar results for similar numbers" do
@@ -39,8 +45,22 @@ describe "Hashing Functions" do
     end
 
     it "should return an integer" do
-      p subject.my_hash
       expect(subject.my_hash).to be_a(Integer)
+    end
+
+    it "should hash in a deterministic way" do
+      expect("hello".my_hash).to eq("hello".my_hash)
+    end
+
+    it "should handle empty string" do
+      expect("".my_hash).not_to eq("")
+    end
+
+    it "should not return similar results for similar strings" do
+      forward, backward = ["hello", "olleh"]
+      a, b = ["hello", "iello"]
+      expect(forward.my_hash.to_s.reverse).not_to eq(backward.to_s.my_hash)
+      expect(a.my_hash * -1).not_to eq(b.my_hash)
     end
   end # of String
 
