c8cb680909874d6f673449cc9be0c2579fe936d1
chrisbrickey <cbrickey1@gmail.com>
2017-10-31 15:26:12 -0700

    write first 2 tests

--- spec_hash/hashset_spec.rb
+++ spec_hash/hashset_spec.rb
@@ -1,13 +1,33 @@
 require "./lib_hash/hashset"
+require "./lib_hash/hashing"
 require "rspec/expectations"
 
 describe "HashSet" do
 
+  before(:all) do
+    subject = HashSet.new
+  end
+
   describe "HashSet#initialize" do
+
+    it "initializes to length of 4" do
+      expect(subject.size).to eq(4)
+    end
 
   end #of initialize
 
   describe "HashSet#insert" do
+
+    it "inserts an element" do
+      element = "a"
+      desired_position = element.my_hash % 4
+      subject.insert(element)
+      expect(subject[desired_position]).to be(element)
+    end
+
+    xit "inserts an element regardless of type" do
+
+    end
 
     xit "runs faster than linear time on average" do
       # slow_subject = SlowDynamicArray.new
@@ -31,6 +51,12 @@ describe "HashSet" do
   end #of remove
 
   describe "HashSet#include?" do
+
+    it "returns true if element is included" do
+    end
+
+    it "returns false if element is not included" do
+    end
 
     xit "runs faster than linear time on average" do
     end
