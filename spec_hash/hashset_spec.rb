6ea05c73faf9393aaa5d322ae696dfd7d3055d56
chrisbrickey <cbrickey1@gmail.com>
2017-10-31 16:44:40 -0700

    write non-time-constrained tests for HashSet#remove

--- spec_hash/hashset_spec.rb
+++ spec_hash/hashset_spec.rb
@@ -22,9 +22,6 @@ describe HashSet do
       element = "a"
       desired_position = element.my_hash % 4
       subject.insert(element)
-
-    p subject.store
-
       expect(subject.store[desired_position][0]).to eq(element)
     end
 
@@ -35,39 +32,30 @@ describe HashSet do
       el7 = [5, "a", -700]
       el8 = { "a" => [4, 5, 6], "b" => "hello" }
 
-
-
       [el1, el2, el3, el4, el5, el6, el7, el8].each do |el|
         desired_position = el.my_hash % subject.size
         subject.insert(el)
         expect(subject.store[desired_position].include?(el)).to be(true)
 
-        p subject.store
+        # p subject.store
       end
 
     end
 
     it "manages collisions; holds multiple elements inserted at the same position" do
       this_size = subject.size
-      position = nil
+
       el1 = "a"
-      el1_position = el1.my_hash % this_size
+      position = el1.my_hash % this_size
       el2 = nil
 
       ("b".."z").each do |letter|
         letter_position = letter.my_hash % this_size
-        if el1_position == letter_position
-          position = el1_position
+        if position == letter_position
           el2 = letter
           break
         end
       end
-
-      # print el1
-      # print el1.my_hash % this_size
-      # print el2
-      # print el2.my_hash % this_size
-      # print el1_position
 
       subject.insert(el1)
       subject.insert(el2)
@@ -91,6 +79,61 @@ describe HashSet do
 
   describe "HashSet#remove" do
 
+    it "removes an element" do
+      element = "a"
+      desired_position = element.my_hash % 4
+      subject.insert(element)
+      subject.remove(element)
+      expect(subject.store[desired_position][0]).to eq([])
+    end
+
+    it "removes an element regardless of type" do
+      el1, el2, el3, el4 = 0, "", [], {}
+      el5 = -73
+      el6 = 42
+      el7 = [5, "a", -700]
+      el8 = { "a" => [4, 5, 6], "b" => "hello" }
+
+      #inserting the elements
+      [el1, el2, el3, el4, el5, el6, el7, el8].each do |el|
+        desired_position = el.my_hash % subject.size
+        subject.insert(el)
+      end
+
+      #removing the elements
+      [el1, el2, el3, el4, el5, el6, el7, el8].each do |el|
+        desired_position = el.my_hash % subject.size
+        subject.remove(el)
+        expect(subject.store[desired_position].include?(el)).to be(false)
+      end
+
+    end
+
+    it "manages collisions; removes only the desired element and leaves others at the same position" do
+      this_size = subject.size
+      position = nil
+      el1 = "a"
+      el1_position = el1.my_hash % this_size
+      el2 = nil
+
+      ("b".."z").each do |letter|
+        letter_position = letter.my_hash % this_size
+        if el1_position == letter_position
+          position = el1_position
+          el2 = letter
+          break
+        end
+      end
+
+      subject.insert(el1)
+      subject.insert(el2)
+      subject.remove(el1)
+      expect(subject.store[position].include?(el1)).to be(false)
+      expect(subject.store[position].include?(el2)).to be(true)
+    end
+
+
+
     xit "runs faster than linear time on average" do
     end
 
@@ -98,10 +141,10 @@ describe HashSet do
 
   describe "HashSet#include?" do
 
-    it "returns true if element is included" do
+    xit "returns true if element is included" do
     end
 
-    it "returns false if element is not included" do
+    xit "returns false if element is not included" do
     end
 
     xit "runs faster than linear time on average" do
