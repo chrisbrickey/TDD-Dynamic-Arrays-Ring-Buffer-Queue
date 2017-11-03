c30d5275b47fef49436e90d68231de14e09c68ad
chrisbrickey <cbrickey1@gmail.com>
2017-11-02 17:01:18 -0700

    setup testing and production files for node + linked_list

---
+++ spec_hash/linked_list_spec.rb
@@ -0,0 +1,18 @@
+require "./lib_hash/node"
+require "./lib_hash/linked_list"
+require "rspec/expectations"
+
+describe LinkedList do
+
+  before(:all) do
+    subject = LinkedList.new
+  end
+
+  describe "LinkedList#initialize" do
+
+    xit "does something" do
+    end
+
+  end
+
+end #of LinkedList
---
+++ spec_hash/node_spec.rb
@@ -0,0 +1,17 @@
+require "./lib_hash/node"
+require "rspec/expectations"
+
+describe Node do
+
+  before(:all) do
+    subject = Node.new
+  end
+
+  describe "Node#initialize" do
+
+    xit "does something" do
+    end
+
+  end
+
+end #of Node
