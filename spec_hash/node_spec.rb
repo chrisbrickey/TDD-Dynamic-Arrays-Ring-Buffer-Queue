a0213e5eaeb31a67414de0e17d4e0dbb8744a253
chrisbrickey <cbrickey1@gmail.com>
2017-11-02 17:17:02 -0700

    add test to reinforce state at Node#initialize

--- spec_hash/node_spec.rb
+++ spec_hash/node_spec.rb
@@ -7,13 +7,23 @@ describe Node do
     subject = Node.new
   end
 
+  describe "Node#initialize" do
+
+    it "initializes all instance variables to nil" do
+      expect(subject.value).to eq(nil)
+      expect(subject.left).to eq(nil)
+      expect(subject.right).to eq(nil)
+    end
+    
+  end
+
   describe "Node#getter/setter" do
 
     it "allows access and assignment to value, left child, and right child" do
       subject.value = 16
       subject.left = 17
       subject.right = 18
-      
+
       expect(subject.value).to eq(16)
       expect(subject.left).to eq(17)
       expect(subject.right).to eq(18)
