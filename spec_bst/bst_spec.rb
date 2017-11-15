49b1e438a10ad6576f1443feebfa5698d1928a61
chrisbrickey <cbrickey1@gmail.com>
2017-11-15 15:45:30 -0800

    establish descriptions for BST#insert specs

--- spec_bst/bst_spec.rb
+++ spec_bst/bst_spec.rb
@@ -1,2 +1,26 @@
 require "./lib_bst/bst"
 require "rspec/expectations"
+
+describe BinarySearchTree do
+
+  before(:all) do
+    subject = BinarySearchTree.new
+  end
+
+  describe "BinarySearchTree#insert" do
+
+    it "inserts at the root if tree is empty" do
+    end
+
+    it "inserts to left of root if value is less than root" do
+    end
+
+    it "inserts to right of root if value is greater than or equal to the root" do
+    end
+
+    it "inserts in the appropriate place given a large tree" do
+    end
+
+  end
+
+end #of Node
