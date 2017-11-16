22789730ebf59949e5557657457ce74cc4d367f0
chrisbrickey <cbrickey1@gmail.com>
2017-11-15 16:24:01 -0800

    implement BST#initialize

---
+++ lib_bst/bst.rb
@@ -0,0 +1,12 @@
+class BinarySearchTree
+
+  attr_accessor :root, :length #delete length after test are developed
+
+  def initialize
+    @length = 0 #for test development ONLY, delete later
+    @root = nil
+  end
+
+
+
+end
