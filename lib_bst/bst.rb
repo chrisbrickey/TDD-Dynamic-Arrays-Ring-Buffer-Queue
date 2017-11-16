727eaf3f4f1ad4b724b48dd45bf1b027c7e0bb32
chrisbrickey <cbrickey1@gmail.com>
2017-11-15 18:36:22 -0800

    clean up BST#insert

--- lib_bst/bst.rb
+++ lib_bst/bst.rb
@@ -9,17 +9,36 @@ class BinarySearchTree
     @root = nil
   end
 
-  def insert(el)
-    new_node = Node.new(el)
-    if @root
-      if new_node.value < @root.value
-        @root.left = new_node
-      else
-        @root.right = new_node
-      end
-    else
-      @root = new_node
+  def insert(el, tree_node=@root)
+
+    # return BSTNode.new(value) if tree_node.nil?
+
+    if (@root.nil?) && (tree_node == @root)
+      @root = Node.new(el)
+      return @root
     end
+
+    if el < tree_node.value
+      if tree_node.left
+        insert(el, tree_node.left)
+      else
+        tree_node.left = Node.new(el)
+      end
+    else # new_node.value >= tree_node.value
+      if tree_node.right
+        insert(el, tree_node.right)
+      else
+        tree_node.right = Node.new(el)
+      end
+    end
+
+
+  end
+
+  def print_tree
+    print "root: #{@root.value}\n"
+    print "l1: #{@root.left.value}   #{@root.right.value}\n"
+    print "l2: #{@root.left.left.value}  #{@root.left.right.value} #{@root.right.left.value}  #{@root.right.right.value}\n"
   end
 
 
