312214431148bab7bbce78142ba65aae8bd97e20
chrisbrickey <cbrickey1@gmail.com>
2017-11-15 16:29:42 -0800

    connect to Node class and implement BST#insert for two levels only

--- lib_bst/bst.rb
+++ lib_bst/bst.rb
@@ -1,3 +1,5 @@
+require_relative 'node'
+
 class BinarySearchTree
 
   attr_accessor :root, :length #delete length after test are developed
@@ -5,6 +7,19 @@ class BinarySearchTree
   def initialize
     @length = 0 #for test development ONLY, delete later
     @root = nil
+  end
+
+  def insert(el)
+    new_node = Node.new(el)
+    if @root
+      if new_node.value < @root.value
+        @root.left = new_node
+      else
+        @root.right = new_node
+      end
+    else
+      @root = new_node
+    end
   end
 
 
