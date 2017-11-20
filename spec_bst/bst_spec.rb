9bf85db264d808b37091d71432265120c6502a04
chrisbrickey <cbrickey1@gmail.com>
2017-11-20 15:49:47 -0800

    draft tests for BST#delete when target has one child

--- spec_bst/bst_spec.rb
+++ spec_bst/bst_spec.rb
@@ -249,6 +249,75 @@ describe BinarySearchTree do
 
     end
 
+    it "when the target has only one child, it restructures the tree correctly" do
+      [10, 8, 12, 6, 7, 14, 13, 51].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     /        \
+      #    6         14
+      #     \        / \
+      #     7       13  51
+
+      subject.delete(12)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      14
+      #     /      / \
+      #    6     13   51
+      #     \
+      #     7
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left #8
+      first_right = subject.root.right #14
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(14)
+      expect(first_right.parent.value).to eq(10)
+
+      expect(first_left.left.value).to eq(6)
+      expect(first_left.left.right.value).to eq(7)
+      expect(first_left.right).to eq(nil)
+
+      expect(first_right.left.value).to eq(13)
+      expect(first_right.right.value).to eq(51)
+      expect(first_right.right.left).to eq(nil)
+      expect(first_right.right.right).to eq(nil)
+
+
+      subject.delete(8)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #     6       14
+      #      \      / \
+      #       7   13   51
+
+      first_left = subject.root.left #6
+      first_right = subject.root.right #14
+      expect(first_left.value).to eq(6)
+      expect(first_left.parent.value).to eq(10)
+      expect(first_right.value).to eq(14)
+
+      expect(first_left.left).to eq(nil)
+      expect(first_left.right.value).to eq(7)
+      expect(first_left.right.right).to eq(nil)
+
+      expect(first_right.left.value).to eq(13)
+      expect(first_right.right.value).to eq(51)
+      expect(first_right.right.left).to eq(nil)
+      expect(first_right.right.right).to eq(nil)
+
+
+
+    end
+
   end
 
 end #of BinarySearchTree
