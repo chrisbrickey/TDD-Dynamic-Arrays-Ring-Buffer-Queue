7808a36a087b21983dca53a8594ebb6e3020ad96
chrisbrickey <cbrickey1@gmail.com>
2017-11-26 23:58:18 -0500

    setup testing file structure and api skeleton

---
+++ spec_stacks/stack_of_plates_spec.rb
@@ -0,0 +1,34 @@
+require "./lib_stacks/stack_of_plates"
+require "rspec/expectations"
+
+describe StackOfPlates do
+
+  describe "#push" do
+
+    it "adds an element to the end" do
+
+    end
+
+  end
+
+  describe "#pop" do
+
+    it "removes an element from the end (same side as push)" do
+
+    end
+
+  end
+
+  describe "#atIndex" do
+
+    it "if the index is occupied, it returns the element at that index" do
+
+    end
+
+    it "if the index is vacant, it returns nil" do
+
+    end
+
+  end
+
+end # of StackOfPlates
