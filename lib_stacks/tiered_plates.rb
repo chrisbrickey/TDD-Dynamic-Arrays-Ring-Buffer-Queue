fe9794d9f2a0bbc8ec41022bdac1d2fdc38bc1d4
chrisbrickey <cbrickey1@gmail.com>
2017-11-27 17:03:06 -0800

    implement naive TieredPlates#push

--- lib_stacks/tiered_plates.rb
+++ lib_stacks/tiered_plates.rb
@@ -1,2 +1,24 @@
 class TieredPlates
+
+  def initialize
+    @stacks = [[nil, nil, nil]]
+    @capacity = 3
+    @num_of_stacks = 1
+    @length = 0
+  end
+
+  def push(element)
+    stack_idx = @num_of_stacks - 1  #0 if still on first stack
+    col_idx = @length % 3
+    @stacks[stack_idx][col_idx] = element
+  end
+
+  def pop()
+    "a"
+  end
+
+
+  def atIndex(idx)
+  end
+
 end # of class
