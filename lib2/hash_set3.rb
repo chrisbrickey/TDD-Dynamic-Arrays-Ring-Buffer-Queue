09270f3723d3d2a3ec24bed9609eec9b76e38464
chrisbrickey <cbrickey1@gmail.com>
2017-12-03 18:30:57 -0800

    change underlying dynamic array to static array and implement resize method

--- lib2/hash_set3.rb
+++ lib2/hash_set3.rb
@@ -1,27 +1,45 @@
 class Set
 
   def initialize
-    @elements = []
+    @elements = Array.new(2)
+    @length = 0
+    @capacity = 1
   end
 
   def isEmpty
-    @elements.length == 0
+    @length == 0
   end
 
   def add(element)
-    @elements << element
+    resize if @length == @capacity 
+    
+    @elements[@length] = element
+    @length += 1
   end
 
   def remove(element)
-    new_elements = []
+    new_elements = Array.new(@capacity)
+    idx = 0
     @elements.each do |el|
       if el != element
-        new_elements << el
+        new_elements[idx] = el
+        idx += 1
+      else
+        @length -= 1
       end
     end
 
     @elements = new_elements
   end
 
+  def resize
+    @capacity *= 2
+    new_elements = Array.new(@capacity)
+    @elements.each_with_index do |el, idx| 
+      new_elements[idx] = el
+    end
+    
+    @elements = new_elements
+  end
 
 end
