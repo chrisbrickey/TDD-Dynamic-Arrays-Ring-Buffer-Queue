83614b7f5ce00a496ca4578fc9cbdea395b60370
chrisbrickey <cbrickey1@gmail.com>
2017-12-12 15:36:39 -0800

    copy basic BST specs and implementation to BalancedBST

---
+++ lib_bst/bst_balanced.rb
@@ -0,0 +1,146 @@
+require_relative 'node'
+
+class BalancedBST
+
+  attr_accessor :root
+
+
+  def initialize
+    @root = nil
+  end
+
+
+  def insert(el, tree_node=@root)
+
+    if (@root.nil?) && (tree_node == @root)
+      @root = Node.new(el)
+      return @root
+    end
+
+    if el < tree_node.value
+      if tree_node.left
+        insert(el, tree_node.left)
+      else
+        new_node = Node.new(el)
+        new_node.parent = tree_node
+        tree_node.left = new_node
+      end
+    else # el >= tree_node.value
+      if tree_node.right
+        insert(el, tree_node.right)
+      else
+        new_node = Node.new(el)
+        new_node.parent = tree_node
+        tree_node.right = new_node
+      end
+    end
+
+  end
+
+
+  def find(target, top_node=@root)
+    return nil if top_node.nil?
+
+    if top_node.value == target
+      return top_node
+    elsif target < top_node.value
+      return find(target, top_node.left)
+    else
+      return find(target, top_node.right)
+    end
+
+  end
+
+
+  def delete(target, top_node=@root)
+    the_node_to_delete = find(target, top_node) #grabs the target but gives no info on location within tree and relative to its parent
+    return nil if the_node_to_delete.nil?
+
+    #find where in the tree the target is
+    the_parent = the_node_to_delete.parent
+    what_we_are_deleting = nil
+    if the_parent.nil?
+      what_we_are_deleting = "root"
+    elsif the_node_to_delete.value < the_parent.value
+      what_we_are_deleting = "left"
+    else
+      what_we_are_deleting = "right"
+    end
+
+    # target has no children so remove from tree and return early
+    if (the_node_to_delete.left.nil?) && (the_node_to_delete.right.nil?)
+      case what_we_are_deleting
+      when "root" #the root was the only node in the tree
+        @root = nil
+      when "left"
+        the_parent.left = nil
+      when "right"
+        the_parent.right = nil
+      end
+
+      the_node_to_delete.parent = nil
+      return the_node_to_delete
+    end
+
+
+    # target has one child
+    if the_node_to_delete.left.nil? || the_node_to_delete.right.nil?
+      replacement_node = the_node_to_delete.left.nil? ? the_node_to_delete.right : the_node_to_delete.left
+
+    # target has two children and the replacement node is a direct child of target (no sibling)
+    # can shift the left subtree up to replace without dismantelling any of the replacement's children
+    elsif the_node_to_delete.left.right.nil?
+      replacement_node = the_node_to_delete.left
+
+      #detach and reattach the right subtree
+      right_child = the_node_to_delete.right
+      right_child.parent = replacement_node
+      replacement_node.right = right_child
+
+
+    # target has two children and the replacement node is NOT a direct child of the_node_to_delete
+    #  replace with max of left subtree (replacement )
+    else
+      replacement_node = the_node_to_delete.left #6
+      until replacement_node.right.nil?
+        replacement_node = replacement_node.right
+      end
+
+      # if replacement node has a child, disconnect and reconnect to replacement's parent on right side
+      replacements_left_child = replacement_node.left
+      if replacements_left_child #...is not nil
+        replacement_node.parent.right = replacements_left_child
+        replacements_left_child.parent = replacement_node.parent
+      end
+
+      #detach and reassign the left subtree
+      left_child = the_node_to_delete.left
+      left_child.parent = replacement_node
+      replacement_node.left = left_child
+
+      #detach and reattach the right subtree
+      right_child = the_node_to_delete.right
+      right_child.parent = replacement_node
+      replacement_node.right = right_child
+    end
+
+    case what_we_are_deleting
+    when "root"
+      @root = replacement_node
+      replacement_node.parent = nil
+    when "left"
+      the_parent.left = replacement_node
+      replacement_node.parent = the_parent
+    when "right"
+      the_parent.right = replacement_node
+      replacement_node.parent = the_parent
+    end
+
+    the_node_to_delete.left = nil
+    the_node_to_delete.right = nil
+    the_node_to_delete.parent = nil
+    the_node_to_delete
+  end
+
+
+end
--- spec_bst/bst_balanced_spec.rb
+++ spec_bst/bst_balanced_spec.rb
@@ -1,2 +1,518 @@
 require "./lib_bst/bst_balanced"
 require "rspec/expectations"
+
+describe BalancedBST do
+
+  before(:all) do
+    subject = BalancedBST.new
+  end
+
+  describe "BalancedBST#insert" do
+
+
+    it "inserts at the root if tree is empty" do
+      expect(subject.root).to eq(nil)
+      subject.insert(10)
+      expect(subject.root.value).to eq(10)
+    end
+
+
+    it "inserts to left of root if value is less than root" do
+        subject.insert(10)
+        subject.insert(8)
+        expect(subject.root.left.value).to eq(8)
+    end
+
+
+    it "inserts to right of root if value is greater than the root" do
+      subject.insert(10)
+      subject.insert(12)
+      expect(subject.root.right.value).to eq(12)
+    end
+
+
+    it "inserts to right of root if value is equal to the root" do
+      subject.insert(10)
+      subject.insert(10)
+      expect(subject.root.right.value).to eq(10)
+    end
+
+
+    it "constructs the appropriate left/right relationships for a large tree" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     / \    /  \
+      #    6  9   11  14
+      #        \      / \
+      #        9    13   51
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left
+      first_right = subject.root.right
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(12)
+
+      expect(first_left.left.value).to eq(6)
+      expect(first_left.right.value).to eq(9)
+      expect(first_left.right.right.value).to eq(9)
+      expect(first_left.right.left).to eq(nil)
+
+      expect(first_right.left.value).to eq(11)
+      expect(first_right.right.value).to eq(14)
+      expect(first_right.right.left.value).to eq(13)
+      expect(first_right.right.right.value).to eq(51)
+
+      expect(first_right.right.right.left).to eq(nil)
+      expect(first_right.right.right.right).to eq(nil)
+    end
+
+
+    it "constructs the appropriate parent relationships for a large tree" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     / \    /  \
+      #    6  9   11  14
+      #        \      / \
+      #        9    13   51
+
+      expect(subject.root.parent).to eq(nil)
+
+      first_left = subject.root.left #8
+      first_right = subject.root.right #12
+      expect(first_left.parent.value).to eq(10)
+      expect(first_right.parent.value).to eq(10)
+
+      expect(first_left.left.parent.value).to eq(8)
+      expect(first_left.right.parent.value).to eq(8)
+      expect(first_left.right.right.parent.value).to eq(9)
+
+      expect(first_right.left.parent.value).to eq(12)
+      expect(first_right.right.parent.value).to eq(12)
+      expect(first_right.right.left.parent.value).to eq(14)
+      expect(first_right.right.right.parent.value).to eq(14)
+    end
+
+  end
+
+
+  describe "BalancedBST#find" do
+
+
+    it "returns nil when value not present in tree" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13].each { |el| subject.insert(el) }
+      expect(subject.find(7)).to eq(nil)
+      expect(subject.find(0)).to eq(nil)
+      expect(subject.find(100)).to eq(nil)
+    end
+
+
+    it "returns the target node when value is present in tree" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     / \    /  \
+      #    6  9   11  14
+      #        \      / \
+      #        9    13   51
+
+      expect(subject.find(10).value).to eq(10)
+      expect(subject.find(8).value).to eq(8)
+      expect(subject.find(12).value).to eq(12)
+      expect(subject.find(6).value).to eq(6)
+      expect(subject.find(9).value).to eq(9)
+      expect(subject.find(11).value).to eq(11)
+      expect(subject.find(14).value).to eq(14)
+      expect(subject.find(13).value).to eq(13)
+      expect(subject.find(51).value).to eq(51)
+    end
+
+  end
+
+
+  describe "BalancedBST#delete" do
+
+    it "when value is not present in tree, it returns nil" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13].each { |el| subject.insert(el) }
+      expect(subject.delete(7)).to eq(nil)
+      expect(subject.delete(0)).to eq(nil)
+      expect(subject.delete(100)).to eq(nil)
+    end
+
+
+    it "when value is not present in tree, it does not alter the structure of the tree" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+      subject.delete(7)
+      subject.delete(0)
+      subject.delete(100)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     / \    /  \
+      #    6  9   11  14
+      #        \      / \
+      #        9    13   51
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left
+      first_right = subject.root.right
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(12)
+
+      expect(first_left.left.value).to eq(6)
+      expect(first_left.right.value).to eq(9)
+      expect(first_left.right.right.value).to eq(9)
+      expect(first_left.right.left).to eq(nil)
+
+      expect(first_right.left.value).to eq(11)
+      expect(first_right.right.value).to eq(14)
+      expect(first_right.right.left.value).to eq(13)
+      expect(first_right.right.right.value).to eq(51)
+
+      expect(first_right.right.right.left).to eq(nil)
+      expect(first_right.right.right.right).to eq(nil)
+    end
+
+
+    it "when value is present in tree, it returns the target node" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13].each { |el| subject.insert(el) }
+      expect(subject.delete(10).value).to eq(10)
+      expect(subject.delete(8).value).to eq(8)
+      expect(subject.delete(11).value).to eq(11)
+      expect(subject.delete(9).value).to eq(9)
+    end
+
+
+    it "when the target is a left leaf, it restructures the tree correctly" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+      subject.delete(6)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #       \    /  \
+      #       9   11  14
+      #        \      / \
+      #        9    13   51
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left
+      first_right = subject.root.right
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(12)
+
+      expect(first_left.left).to eq(nil)
+      expect(first_left.right.value).to eq(9)
+      expect(first_left.right.right.value).to eq(9)
+      expect(first_left.right.left).to eq(nil)
+
+      expect(first_right.left.value).to eq(11)
+      expect(first_right.right.value).to eq(14)
+      expect(first_right.right.left.value).to eq(13)
+      expect(first_right.right.right.value).to eq(51)
+
+      expect(first_right.right.right.left).to eq(nil)
+      expect(first_right.right.right.right).to eq(nil)
+    end
+
+
+    it "when the target is a right leaf, it restructures the tree correctly" do
+      [10, 8, 12, 11, 6, 9, 14, 9, 13, 51].each { |el| subject.insert(el) }
+      subject.delete(51)
+
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     / \    /  \
+      #    6  9   11  14
+      #        \      /
+      #        9     13
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left
+      first_right = subject.root.right
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(12)
+
+      expect(first_left.left.value).to eq(6)
+      expect(first_left.right.value).to eq(9)
+      expect(first_left.right.right.value).to eq(9)
+      expect(first_left.right.left).to eq(nil)
+
+      expect(first_right.left.value).to eq(11)
+      expect(first_right.right.value).to eq(14)
+      expect(first_right.right.left.value).to eq(13)
+      expect(first_right.right.right).to eq(nil)
+    end
+
+
+    it "when the target has only one child, it restructures the tree correctly" do
+      [10, 8, 12, 6, 7, 14, 13, 51].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      12
+      #     /        \
+      #    6         14
+      #     \        / \
+      #     7       13  51
+
+      subject.delete(12)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      8      14
+      #     /      / \
+      #    6     13   51
+      #     \
+      #     7
+
+      expect(subject.root.value).to eq(10)
+
+      first_left = subject.root.left #8
+      first_right = subject.root.right #14
+      expect(first_left.value).to eq(8)
+      expect(first_right.value).to eq(14)
+      expect(first_right.parent.value).to eq(10)
+
+      expect(first_left.left.value).to eq(6)
+      expect(first_left.left.right.value).to eq(7)
+      expect(first_left.right).to eq(nil)
+
+      expect(first_right.left.value).to eq(13)
+      expect(first_right.right.value).to eq(51)
+      expect(first_right.right.left).to eq(nil)
+      expect(first_right.right.right).to eq(nil)
+
+
+      subject.delete(8)
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #     6       14
+      #      \      / \
+      #       7   13   51
+
+      first_left = subject.root.left #6
+      first_right = subject.root.right #14
+      expect(first_left.value).to eq(6)
+      expect(first_left.parent.value).to eq(10)
+      expect(first_right.value).to eq(14)
+
+      expect(first_left.left).to eq(nil)
+      expect(first_left.right.value).to eq(7)
+      expect(first_left.right.right).to eq(nil)
+
+      expect(first_right.left.value).to eq(13)
+      expect(first_right.right.value).to eq(51)
+      expect(first_right.right.left).to eq(nil)
+      expect(first_right.right.right).to eq(nil)
+    end
+
+
+    it "when the target has only one child and target is root, it restructures the tree correctly" do
+      [10, 12, 14, 13, 51].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #         10
+      #           \
+      #            12
+      #             \
+      #             14
+      #            / \
+      #          13  51
+
+      subject.delete(10)
+
+      #    expected structure:
+      #            12
+      #             \
+      #             14
+      #            / \
+      #          13  51
+
+      expect(subject.root.value).to eq(12)
+      expect(subject.root.parent).to eq(nil)
+
+      expect(subject.root.left).to eq(nil)
+      expect(subject.root.right.value).to eq(14)
+
+      expect(subject.root.right.left.value).to eq(13)
+      expect(subject.root.right.right.value).to eq(51)
+    end
+
+    it "when target has 2 children and left child is max of left subtree, it restructures the tree correctly" do
+      [10, 8, 14, 6, 9, 12, 16, 4, 5].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #          10
+      #       /       \
+      #      8         14
+      #     / \      /     \
+      #    6  9     12      16
+      #   /
+      #  4
+      #   \
+      #    5
+
+      subject.delete(8)
+
+      #    expected structure:
+      #          10
+      #       /       \
+      #      6         14
+      #     / \      /     \
+      #    4  9     12      16
+      #     \
+      #      5
+
+      expect(subject.root.value).to eq(10)
+      expect(subject.root.left.value).to eq(6)
+      expect(subject.root.left.parent.value).to eq(10)
+
+      expect(subject.root.left.left.value).to eq(4)
+      expect(subject.root.left.right.value).to eq(9)
+      expect(subject.root.left.right.parent.value).to eq(6)
+
+      expect(subject.root.left.right.right).to eq(nil)
+      expect(subject.root.left.left.left).to eq(nil)
+      expect(subject.root.left.left.right.value).to eq(5)
+    end
+
+
+    it "when the target has two children, it restructures the tree correctly" do
+      [10, 8, 14, 6, 9, 12, 16, 7, 6.5, 4, 15, 13, 11, 12.5].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #          10
+      #       /       \
+      #      8         14
+      #     / \      /     \
+      #    6  9     12      16
+      #   / \      / \      /
+      #  4   7    11  13  15
+      #     /         /
+      #    6.5      12.5
+
+      subject.delete(8)
+
+      #    expected structure:
+      #          10
+      #       /       \
+      #      7          14
+      #    /  \       /   \
+      #   6   9      12      16
+      #  / \        / \     /
+      # 4   6.5    11  13  15
+      #                /
+      #              12.5
+
+      expect(subject.root.value).to eq(10)
+      expect(subject.root.left.value).to eq(7)
+      expect(subject.root.left.parent.value).to eq(10)
+
+      expect(subject.root.left.left.value).to eq(6)  #where we are failing
+      expect(subject.root.left.left.parent.value).to eq(7)
+      expect(subject.root.left.right.value).to eq(9)
+      expect(subject.root.left.right.parent.value).to eq(7)
+
+      expect(subject.root.left.right.right).to eq(nil)
+      expect(subject.root.left.left.left.value).to eq(4)
+      expect(subject.root.left.left.right.value).to eq(6.5)
+      expect(subject.root.left.left.right.left).to eq(nil)
+      expect(subject.root.left.left.right.right).to eq(nil)
+
+
+      subject.delete(14)
+
+      #    expected structure:
+      #           10
+      #       /        \
+      #      7           13
+      #     / \       /      \
+      #    6  9      12        16
+      #   / \       /  \      /
+      #  4  6.5    11  12.5  15
+
+      expect(subject.root.right.value).to eq(13)
+      expect(subject.root.right.parent.value).to eq(10)
+
+      expect(subject.root.right.left.value).to eq(12)
+      expect(subject.root.right.left.parent.value).to eq(13)
+      expect(subject.root.right.right.value).to eq(16)
+      expect(subject.root.right.right.parent.value).to eq(13)
+
+      expect(subject.root.right.left.left.value).to eq(11)
+      expect(subject.root.right.left.right.value).to eq(12.5)
+      expect(subject.root.right.left.right.left).to eq(nil)
+
+      expect(subject.root.right.right.right).to eq(nil)
+      expect(subject.root.right.right.left.value).to eq(15)
+
+    end
+
+    it "when the target is the root and it has two children, it restructures the tree correctly" do
+      [10, 6, 12, 4, 8, 7, 11, 14].each { |el| subject.insert(el) }
+
+      #    expected structure:
+      #         10
+      #       /    \
+      #      6      12
+      #     / \    /  \
+      #    4   8  11  14
+      #       /
+      #      7
+
+      subject.delete(10)
+
+      #    expected structure:
+      #          8
+      #       /     \
+      #      6       12
+      #     / \     /  \
+      #    4   7    11  14
+
+      expect(subject.root.value).to eq(8)
+      expect(subject.root.parent).to eq(nil)
+
+      first_left = subject.root.left
+      first_right = subject.root.right
+      expect(first_left.value).to eq(6)
+      expect(first_left.parent.value).to eq(8)
+      expect(first_right.value).to eq(12)
+      expect(first_right.parent.value).to eq(8)
+
+      expect(first_left.left.value).to eq(4)
+      expect(first_left.right.value).to eq(7)
+      expect(first_left.right.right).to eq(nil)
+      expect(first_left.right.left).to eq(nil)
+
+      expect(first_right.left.value).to eq(11)
+      expect(first_right.right.value).to eq(14)
+
+    end
+
+
+  end
+
+end #of BalancedBST
