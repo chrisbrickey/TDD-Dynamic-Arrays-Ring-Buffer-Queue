7e1d6332816dbbd2a878590685d8bbfad78a744e
chrisbrickey <cbrickey1@gmail.com>
2018-01-29 20:21:52 -0800

    make #depth and #is_balanced? private methods for BalancedBST

--- lib_bst/bst_balanced.rb
+++ lib_bst/bst_balanced.rb
@@ -14,7 +14,7 @@ class BalancedBST
       print "inside rebalance conditional===========\n"
       sorted_arr = traverse_in_order
       size = sorted_arr.length
-      p sorted_arr
+      # p sorted_arr
       mid = size / 2
       @root = Node.new(sorted_arr[mid])
       # insert(sorted_arr[mid])
@@ -26,16 +26,16 @@ class BalancedBST
       # righty = sorted_arr[mid + delta]
       # while !lefty.nil? || !righty.nil?
       while left_idx >= 0 || right_idx < size
-        p "lefty: #{sorted_arr[left_idx]}\n"
-        p "righty: #{sorted_arr[right_idx]}\n"
+        # p "lefty: #{sorted_arr[left_idx]}\n"
+        # p "righty: #{sorted_arr[right_idx]}\n"
 
         if left_idx >= 0
-          p "inserting lefty now..."
+          # p "inserting lefty now..."
           insert(sorted_arr[left_idx])
         end
 
         if right_idx < size
-          p "inserting righty now..."
+          # p "inserting righty now..."
           insert(sorted_arr[right_idx])
         end
         delta += 1
@@ -63,37 +63,7 @@ class BalancedBST
     sorted_arr
   end
 
-  def is_balanced?
-    return true if @root.nil?
 
-    left_depth = depth(@root.left)
-    right_depth = depth(@root.right)
-    delta = (left_depth - right_depth).abs
-
-    delta <= 1 #implicit return
-  end
-
-  def depth(node=@root) #counting levels using bfs
-    depth = 0
-    return depth if node.nil?
-
-    queue = [node]
-
-    while !queue.empty?
-      depth += 1
-      nodes_at_this_level = queue.length
-
-      #remove all nodes and current level and add all nodes at next level
-      nodes_at_this_level.times do
-        current = queue.shift
-        queue << current.left if !current.left.nil?
-        queue << current.right if !current.right.nil?
-      end
-
-    end
-
-    depth
-  end
 
 
   def insert(el, tree_node=@root)
@@ -230,4 +200,38 @@ class BalancedBST
   end
 
 
-end
+  private
+
+  def is_balanced?
+    return true if @root.nil?
+
+    left_depth = depth(@root.left)
+    right_depth = depth(@root.right)
+    delta = (left_depth - right_depth).abs
+
+    delta <= 1 #implicit return
+  end
+
+  def depth(node=@root) #counting levels using bfs
+    depth = 0
+    return depth if node.nil?
+
+    queue = [node]
+
+    while !queue.empty?
+      depth += 1
+      nodes_at_this_level = queue.length
+
+      #remove all nodes and current level and add all nodes at next level
+      nodes_at_this_level.times do
+        current = queue.shift
+        queue << current.left if !current.left.nil?
+        queue << current.right if !current.right.nil?
+      end
+
+    end
+
+    depth
+  end
+
+end #of BalancedBST
